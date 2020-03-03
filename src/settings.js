099ac75493365bc65ad11dea77415672f2be64c0
YIjun Chen <BenjaminCHEN2016@outlook.com>
2020-03-03 23:47:31 +0800

    feat: framework client"

---
+++ src/settings.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  title: 'AchieveIt 项目管理平台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true
+}
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <p>点击侧边栏刷新页面，上方keep-alive tab，这部分可以自定义内容</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>

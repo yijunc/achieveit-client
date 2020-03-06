96575e0e602d2836e8bc6a323613409613ad4e63
YIjun Chen <BenjaminCHEN2016@outlook.com>
2020-03-06 15:20:57 +0800

    .

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">AchieveIt 项目管理平台</h3>
       </div>
 
       <el-form-item prop="username">
@@ -41,9 +41,10 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
       <div class="tips">
+        <span> G03 @ 2020 软件开发实践</span>
         <span style="margin-right:20px;">username: admin</span>
         <span> password: any</span>
       </div>
